4e48b3c1dbe47e9cb4eee9ee7c2db29a256c916d
Ilona Kozak <ilonenko@gmail.com>
2019-11-24 17:38:38 +0200

    add testing

---
+++ src/test/java/CalcTest.java
@@ -0,0 +1,62 @@
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.rules.TemporaryFolder;
+import org.junit.rules.Timeout;
+import packetyk.OperationType;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Optional;
+
+public class CalcTest {
+
+    @Test
+    public void dividingZero() {
+        double asDouble = OperationType.DIVISION.getOperator().applyAsDouble(3, 0);
+        Double aThrow = Optional.of(asDouble).orElseThrow(IllegalArgumentException::new);
+        System.out.println(aThrow);
+        Assert.assertEquals("You are not dividing on zero!", "Infinity", aThrow.toString());
+    }
+
+    @Test
+    public void correctResult(){
+        double left = 4;
+        double right = 72;
+        double multiplication = (4 * 72);
+        double asDouble = OperationType.MULTIPLICATION.getOperator().applyAsDouble(left, right);
+        Optional<Double> passOpt = Optional.of(asDouble);
+        boolean correctMultiplication = passOpt.filter(mult -> mult.equals(multiplication)).isPresent();
+        Assert.assertTrue("There is an incorrect multiplication!", correctMultiplication);
+    }
+
+    @Test(timeout = 1000)
+    public void infinity() {
+        while (true);
+    }
+
+    @Rule
+    public final TemporaryFolder folder = new TemporaryFolder();
+    @Rule
+    public final Timeout timeout = new Timeout(1000);
+    @Rule
+    public final ExpectedException thrown = ExpectedException.none();
+
+    @Ignore
+    @Test
+    public void anotherInfinity() {
+        while (true);
+    }
+    @Test
+    public void testFileWriting() throws IOException {
+        final File log = folder.newFile("debug.log");
+        final FileWriter logWriter = new FileWriter(log);
+        logWriter.append("Hello, ");
+        logWriter.append("World!!!");
+        logWriter.flush();
+        logWriter.close();
+    }
+}
